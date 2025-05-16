231676c371b9fa84f42342e44e6a94dc912ed3f9
Nikhilesh-H <nikgrinding@gmail.com>
2025-05-16 16:03:56 +0530

    16/05/25

---
+++ 0049 - Group Anagrams/README.md
@@ -0,0 +1,19 @@
+# 49. Group Anagrams
+
+## Problem
+Given an array of strings `strs`, group the anagrams together.
+
+## Thought process
+Two strings are anagrams if they have the same length and the frequency of each character in both strings is identical. So, we can create a hashtable to store the frequency of characters in each string and then use this to group them.
+
+## Approach
+- Create a dictionary to act as a hashtable
+- Traverse through each string in `strs`
+- For each string, calculate the frequency of its characters and use this frequency as the key in the hash table.
+- If the key exists, append the string to its value.
+- If not, add a new entry in the hash table with the frequency as the key and the string in a new list as the value.
+
+## Complexity
+- Time: O(n*k)
+- Space: O(n*k)
+{where k is the length of the longest string in `strs`}
---
+++ 0049 - Group Anagrams/solution.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hashtable = {}
+        for string in strs:
+            l = [0]*26
+            for char in string: l[ord(char) - ord('a')] += 1
+            key = tuple(l)
+            if key not in hashtable: hashtable[key] = [string]
+            else: hashtable[key].append(string)
+        return list(hashtable.values())
+
+if __name__ == "__main__":
+    
+    try:
+        ip = []
+        op = []
+
+        with open(r"LEETCODE\0049 - Group Anagrams\test.txt") as f:
+            line = f.readline()
+            while line:
+                if "Input" in line:
+                    l_str = line.split()[-1][1:-1]
+                    l_list = [i[1:-1] for i in l_str.split(",")]
+                    ip.append(l_list)
+                elif "Output" in line:
+                    op.append(line.split()[-1])
+                line = f.readline()
+
+        soln = Solution()
+        for i in range(len(ip)):
+            print("\nInput:", ip[i])
+            print("Expected Output:", op[i])
+            print("Generated Output:", soln.groupAnagrams(ip[i]))
+        print()
+
+    except Exception as e:
+        print(e)
+        pass
